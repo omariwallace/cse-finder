b0e189abe63f5ce2de80f6c2886e881184255a64
Omari Wallace <omari@qubit.com>
2017-01-30 15:34:45 -0500

    updated user IDs

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 export const endpoint = 'https://app.asana.com/api/1.0/'
 
-export const qubitWorkspacdID = '896401739841'
+export const qubitWorkspaceId = '896401739841'
 
 export const cacheTime = 5
 
 export const minDays = 7
 
 export const cses = [
-  { name: 'Sam T.', id: '42303118585834' },
-  { name: 'Omari', id: '12285891751873' },
-  { name: 'Gavin', id: '27044500382964' },
-  { name: 'Derek', id: '34194097984278' },
-  { name: 'Silvia', id: '94855690156214' },
-  { name: 'Josh', id: '95150761686539' },
-  { name: 'Sam C.', id: '135651286078592' }
+  { name: 'Sam T.', id: '42303118585829' },
+  { name: 'Omari', id: '12285891597188' },
+  { name: 'Gavin', id: '27044500382959' },
+  { name: 'Derek', id: '34194097198463' },
+  { name: 'Silvia', id: '94855690156209' },
+  { name: 'Josh', id: '95150761686534' },
+  { name: 'Sam C.', id: '135651286078587' }
 ]
