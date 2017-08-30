2e747ee538d458972353efa2ffc3931fa90afa66
oZ0614 <omari.wallace@gmail.com>
2017-08-30 18:35:16 -0400

    add nick, remove sams

--- src/config.js
+++ src/config.js
@@ -7,10 +7,9 @@ export const cacheTime = 5
 export const minDays = 7
 
 export const cses = [
-  { name: 'Sam T.', id: '42303118585829' },
   { name: 'Omari', id: '12285891597188' },
   { name: 'Gavin', id: '27044500382959' },
   { name: 'Derek', id: '34194097198463' },
   { name: 'Josh', id: '95150761686534' },
-  { name: 'Sam C.', id: '135651286078587' }
+  { name: 'Nick', id: '406535629543277' }
 ]
