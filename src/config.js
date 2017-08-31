79f9251947783cf7d12591b9ecafef5dc11a1bdc
oZ0614 <omari.wallace@gmail.com>
2017-08-30 23:36:30 -0400

    add list id for header name click

--- src/config.js
+++ src/config.js
@@ -7,9 +7,9 @@ export const cacheTime = 5
 export const minDays = 7
 
 export const cses = [
-  { name: 'Omari', id: '12285891597188' },
-  { name: 'Gavin', id: '27044500382959' },
-  { name: 'Derek', id: '34194097198463' },
-  { name: 'Josh', id: '95150761686534' },
-  { name: 'Nick', id: '406535629543273' }
+  { name: 'Omari', id: '12285891597188', listId: '12285891751873' },
+  { name: 'Gavin', id: '27044500382959', listId: '27044500382964' },
+  { name: 'Derek', id: '34194097198463', listId: '34194097984278' },
+  { name: 'Josh', id: '95150761686534', listId: '95150761686539' },
+  { name: 'Nick', id: '406535629543273', listId: '406535629543277' }
 ]
